d922f95256c985deecf72286b4716228628ab7a8
Noah Yek <ninjayek@gmail.com>
2018-12-27 16:16:49 +0800

    First commit

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import { Button } from 'react-native';
+
+// import Login from '../pages/Login';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      {/* <Login/> */}
+
+      
+
+      <Text>Username</Text>
+      <TextInput
+      style={{ height: 40, width: "95%", borderColor: 'gray', borderWidth: 1,  marginBottom: 20 }}
+      // Adding hint in TextInput using Placeholder option.
+      placeholder="Enter Your Username"
+      // Making the Under line Transparent.
+      underlineColorAndroid="transparent"
+      />
+      <Text>Password</Text>
+      <TextInput 
+      style={{ height: 40, width: "95%", borderColor: 'gray', borderWidth: 1,  marginBottom: 20 }}
+      placeholder="Enter Your password"
+      underlineColorAndroid="transparent"
+      />
+      <Button
+      onPress={this.props.onLoginPress}
+      title="Sign in"
+      color="#841584"
+      // accessibilityLabel="Learn more about this purple button"
+      />
+      </View>
+      
+    );
+    
+  }
+  
+
+  
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
