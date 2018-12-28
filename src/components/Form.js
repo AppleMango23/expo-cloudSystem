6632fb4eedef52b38ff68e446da02bbf1068c813
Noah Yek <ninjayek@gmail.com>
2018-12-28 12:31:52 +0800

    Better textInput 2.0

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,8 @@
 import React,{Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity} from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { Button } from 'react-native';
+
 
 export default class Form extends React.Component {
     render() {
@@ -10,24 +11,28 @@ export default class Form extends React.Component {
 
           <Text style="color:white;">Username</Text>
             <TextInput
-            style={{ height: 50, width: 270, borderColor: 'white', borderWidth: 1,  marginBottom: 20 }}
+            style={{ height: 50, width: 270, borderColor: 'white', borderWidth: 1,  marginBottom: 20, borderRadius:20,backgroundColor:'white' ,placeholderTextColor:'white',textAlign:"center"}}
             // Adding hint in TextInput using Placeholder option.
             placeholder="Enter Your Username"
             // Making the Under line Transparent.
             underlineColorAndroid="transparent"
             />
             <Text>Password</Text>
-            <TextInput 
-            style={{ height: 50, width: 270, borderColor: 'white', borderWidth: 1,  marginBottom: 20 }}
+            <TextInput secureTextEntry={true}
+            style={{ height: 50, width: 270, borderColor: 'white', borderWidth: 1,  marginBottom: 20, borderRadius:20,backgroundColor:'white' ,placeholderTextColor:'white',textAlign:"center"}}
             placeholder="Enter Your password"
             underlineColorAndroid="transparent"
             />
-            <Button
+            {/* <Button
             onPress={this.props.onLoginPress}
             title="Sign in"
             color="#841584"
             // accessibilityLabel="Learn more about this purple button"
-            />
+            /> */}
+
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
           
         </View>
         
@@ -49,5 +54,18 @@ export default class Form extends React.Component {
       width:250,
       borderColor:'gray',
 
+    },
+    button:{
+      width:300,
+      backgroundColor:'#707070',
+      borderRadius:25,
+      marginVertical:10,
+
+    },
+    buttonText:{
+      fontSize:16,
+      fontWeight:'500',
+      color:'#ffffff',
+      textAlign:'center',
     }
   });
